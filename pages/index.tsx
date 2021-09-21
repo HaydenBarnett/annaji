21e7467a14613bbd37fbedba3c323cb989e103a7
Hayden Barnett <hello@haydenbarnett.com>
2021-09-21 21:46:11 +1000

    Add link and fade animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,59 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
 const Home: NextPage = () => {
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
   return (
     <>
       <Head>
         <title>Anna Ji - Head of Product</title>
         <meta
           name="description"
-          content="Anna Ji is the Head of Product at Clipchamp, working to bring video editing online and accessible to everyone."
+          content="Anna Ji is the Head of Product of Clipchamp at Microsoft, working to bring video editing online and accessible to everyone."
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="wrapper">
-        <div className="container">
+        <div className="container" style={{ opacity: imageIsLoaded ? 1 : 0 }}>
           <div className="avatar">
-            <Image src="/avatar.jpg" width="110" height="110" alt="Anna Ji" />
+            <Image
+              src="/avatar.jpg"
+              width="110"
+              height="110"
+              alt="Anna Ji"
+              onLoad={(e) => {
+                const target = e.target as HTMLImageElement;
+                if (target.src.indexOf("data:image/gif;base64") < 0) {
+                  setImageIsLoaded(true);
+                }
+              }}
+            />
           </div>
           <h1>Anna Ji</h1>
           <p>
-            Head of Product at{" "}
+            Head of Product of{" "}
             <Link href="https://clipchamp.com" passHref>
-              <a target="_blank">Clipchamp</a>
-            </Link>
-            , working to bring video editing online and accessible to everyone.
+              <a className="link" target="_blank">
+                Clipchamp
+              </a>
+            </Link>{" "}
+            at Microsoft, working to bring video editing online and accessible
+            to everyone.
           </p>
           <p>
             You can connect with me on{" "}
             <Link href="https://www.linkedin.com/in/annahji" passHref>
-              <a target="_blank">LinkedIn</a>
+              <a className="link" target="_blank">
+                LinkedIn
+              </a>
             </Link>
             , or reach me by email -{" "}
-            <a href="mailto:anna@clipchamp.com">anna@clipchamp.com</a>
+            <a className="link" href="mailto:anna@clipchamp.com">
+              anna@clipchamp.com
+            </a>
           </p>
         </div>
       </main>
